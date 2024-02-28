78f16b5d6f4b4e0d8158e3f8e889d9d692c1904d
Giovanni Correra <146703929+Jinx-Coding@users.noreply.github.com>
2024-02-28 13:55:16 +0100

    Add files via upload
    Second version, pressure added

--- main.py
+++ main.py
@@ -150,6 +150,26 @@ def data(n, feed, feedOm, steamT, finalOm, finalT, feedT):
     return Z, V1, V2, V3, L3, om1, om2, A, U1, U2, U3, S
 
 
+# Saturation Pressure first guess #
+
+def saturation(tmp):
+
+    tmp = tmp - 273.15
+    P = 0.000135*tmp**3 - 0.00552*tmp**2 + 0.175918*tmp + 0.207651  # (kPa) #
+
+    return P
+
+
+# Tomato Paste saturation pressure #
+
+def boiling_point(P, T, om):
+
+    Ts = 18.536*numpy.log(P) + 5.5052  # (C) #
+    f = T - Ts - (0.175*om**1.11)*numpy.exp(3.86*om)*(1000*P/101325)**0.2898
+
+    return f
+
+
 # Solution #
 
 sol0 = numpy.array((TF+(T3-TF)/3, TF+2*(T3-TF)/3, 0.75*F, 0.5*F))
@@ -160,6 +180,10 @@ liquid1 = float(sol[2])
 liquid2 = float(sol[3])
 system_parameters = data(sol, F, omF, TS, om3, T3, TF)
 zeros = system_parameters[0]
+P0 = numpy.array((saturation(temp1), saturation(temp2), saturation(T3)))
+pressure1 = (fsolve(lambda P1: boiling_point(P1, temp1-273.15, system_parameters[5]), P0[0]))
+pressure2 = (fsolve(lambda P2: boiling_point(P2, temp2-273.15, system_parameters[6]), P0[1]))
+pressure3 = (fsolve(lambda P3: boiling_point(P3, T3-273.15, om3), P0[2]))
 
 # Post Processing #
 
@@ -184,6 +208,8 @@ print('   om1 = ', end="")
 print('%.3f' % system_parameters[5], end=" [kg/kg]")
 print('   T1 = ', end="")
 print('%.2f' % (temp1-273.15), end=" [C]")
+print('   P1 = ', end="")
+print('%.2f' % float(pressure1[0]), end="  [kPa]")
 print('   U1 = ', end="")
 print('%.2f' % system_parameters[8], end=" [W/m2K]\n")
 print('L2 = ', end="")
@@ -194,6 +220,8 @@ print('   om2 = ', end="")
 print('%.3f' % system_parameters[6], end=" [kg/kg]")
 print('   T2 = ', end="")
 print('%.2f' % (temp2-273.15), end=" [C]")
+print('   P2 = ', end="")
+print('%.2f' % float(pressure2[0]), end="  [kPa]")
 print('   U2 = ', end="")
 print('%.2f' % system_parameters[9], end=" [W/m2K]\n")
 print('L2 =  ', end="")
@@ -204,5 +232,7 @@ print('   om3 = ', end="")
 print('%.3f' % om3, end=" [kg/kg]")
 print('   T3 = ', end="")
 print('%.2f' % (T3-273.15), end=" [C]")
+print('   P3 = ', end="")
+print('%.2f' % float(pressure3[0]), end="  [kPa]")
 print('   U2 = ', end="")
 print('%.2f' % system_parameters[10], end=" [W/m2K]\n")
